f6c34a08c9a8f3380ef10085535dff73890b3b0b
Shrujan Kotturi <skotturi@uncc.edu>
2016-12-01 19:09:52 -0500

    Scheduler added

--- routes/scheduler.js
+++ routes/scheduler.js
@@ -6,7 +6,7 @@ var db = require('../db'),
 function run() {
 
     //DailyLogs
-    var currentDate = new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDay();
+    var currentDate = new Date().getFullYear() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getDate();
     // var sqlQuery = "SELECT userUserId AS Users FROM dailyFoodLogs WHERE DATE(dateTimeLogged) <= " + currentDate;
     var sqlQuery = "SELECT DISTINCT(D.userUserId) AS Users, UDM.fcmToken AS Token FROM dailyFoodLogs AS D INNER JOIN users U ON U.userId = D.userUserId INNER JOIN userdevicemappers UDM ON UDM.userUserId = U.userId WHERE DATE(D.dateTimeLogged) < '" + currentDate + "' AND U.appNotifications = 1";
 
@@ -16,6 +16,7 @@ function run() {
         result.users = results[0];
         var notificationToUser = "You haven\'t logged your daily food today. Please log it";
         for (var i = 0; i < results[0].length; i++) {
+            console.log("Result : " + util.inspect(results[0][i]));
             var payloadOk = {
                 to: results[0][i].Token,
                 priority: 'high',
